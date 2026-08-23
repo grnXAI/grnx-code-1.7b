80f7c1441d3fc303994f128b2b50d07853bd6cf1
Guillem Romero Naranjo <gromeronaranjo@Guillems-MacBook-Pro.local>
2026-08-24 01:30:55 +0200

    optimizing the data sft download

--- data/processing/download_sft.py
+++ data/processing/download_sft.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset, Dataset
+from tqdm import tqdm
 
 dataset = load_dataset(
     "nvidia/OpenCodeReasoning-2",
@@ -7,16 +8,17 @@ dataset = load_dataset(
     streaming=True
 )
 
-dataset = dataset.shuffle(seed=42, buffer_size=10000)
 samples = []
-
+bar = tqdm(total=50000)
 for row in dataset:
     if float(row["pass_rate"]) == 1.0:
         samples.append(row)
+        bar.update(1)
 
-    if len(samples) == 50000:
+    if len(samples) >= 50000:
         break
 
+bar.close()
 Dataset.from_list(samples).save_to_disk(
     "data/raw/sft/opencode-reasoning-50k"
 )
